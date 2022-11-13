7b3b0c95f8dbbca75fd2d61076ebe3cd1ae47242
Ekrem Şamil Bolat <ekremsamil.1@outlook.com>
2022-11-13 17:17:08 +0300

    Sozluk Uygulamasi

---
+++ lib/Detaysayfa.dart
@@ -0,0 +1,40 @@
+import 'package:dictionary_app/Kelimeler.dart';
+import 'package:flutter/material.dart';
+
+class DetaySayfa extends StatefulWidget {
+  Kelimeler kelime;
+
+  DetaySayfa({required this.kelime});
+
+  @override
+  _DetaySayfaState createState() => _DetaySayfaState();
+}
+
+class _DetaySayfaState extends State<DetaySayfa> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Detay Sayfa"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Text(
+              widget.kelime.ingilizce,
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 40,
+                  color: Colors.pink),
+            ),
+            Text(
+              widget.kelime.turkce,
+              style: TextStyle(fontSize: 40),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
